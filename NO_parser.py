a883aa9ab71a70b4f84773e79f6a00fdd612fb3c
Khyati Paghdar Dholaria <82007550+Khyati-Dholaria@users.noreply.github.com>
2025-05-12 12:57:11 -0400

    Update NO_parser.py (dataframe name issue resolved)

--- NO_parser.py
+++ NO_parser.py
@@ -933,7 +933,7 @@ def create_ss_dfs(
     df_train.name = "TRA"
     if duplicate_pit:
         [df_pit, df_ssp, df_train] = delete_duplicate_pit(df_pit, df_ssp, df_train)
-    df_ssp = df_ssp.reset_index()
+    df_ssp.name = "SSP"
     # Add title to segments in df_pit
     # TODO Add title name to PIT from segment_data to Segment number
     # Create df_ssa from a sub-set of the df_pit, which includes all sub-segments
