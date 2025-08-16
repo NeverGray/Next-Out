c8f35286d349ca2dafd1ad04308d5fb00a60c372
Justin M Edenbaum <62964314+NeverGray@users.noreply.github.com>
2025-08-16 13:16:00 -0400

    Release version 1.3.1: Bug fixes and improvements
    - Fixed train bug when creating Visio diagrams in v1.3
    - Fix Form 4 issues with no fire or Fire Simulation option equal zero
    - Enable better compiling on all computers by removing hard-coded directories. Let's all work together better!
    
    Squashed commit of the following:
    
    commit 6b4cfa2f2b846d68f99621a34304e8ee8341fa6a
    Merge: 8cdd41e e5da963
    Author: Justin Edenbaum <j.edenbaum@italferr.it>
    Date:   Thu Aug 14 17:12:40 2025 -0400
    
        Merge branch 'Bug_fix' of https://github.com/NeverGray/Next-Out into Bug_fix
    
    commit 8cdd41eb34e87e3f72a496debd8d8c155fd07ab7
    Author: Justin Edenbaum <j.edenbaum@italferr.it>
    Date:   Thu Aug 14 11:07:39 2025 -0400
    
        Fix Form 4 issue with multiple simulations
    
    commit e5da963a2f62424796465553c54ddddcd9310850
    Author: Justin Edenbaum <j.edenbaum@italferr.it>
    Date:   Thu Aug 14 11:07:39 2025 -0400
    
        Fix Form 4 issue with multiple simulations
    
    commit 1a9db061d2dd72c68c5c11e824cc5a12cfc007cc
    Author: Justin Edenbaum <j.edenbaum@italferr.it>
    Date:   Thu Aug 14 10:11:47 2025 -0400
    
        Fixed train bug when creating visio diagrams in v1.3.
    
    commit b5558ddb499533ce8ed5a2c64c8339972cec9b44
    Author: Justin Edenbaum <j.edenbaum@italferr.it>
    Date:   Thu Aug 14 09:48:20 2025 -0400
    
        Fix Form 4 issues with no fire simulation

--- NO_parser.py
+++ NO_parser.py
@@ -120,8 +120,8 @@ INPUT = {
     "f1c": re.compile(
         r"SUPPLEMENTARY OUTPUT OPTION\s+(?P<supplement_option>\d)"
     ),
-    "f1c": re.compile(
-        r"SUPPLEMENTARY OUTPUT OPTION\s+(?P<supplement_option>\d)"
+    "f1g_fire": re.compile(
+        r"FIRE SIMULATION OPTION\s+(?P<fire_simulation_option>\d)"
     ),
     "f3a": re.compile(
         r"INPUT VERIFICATION FOR (?P<type>LINE SEGMENT|VENTILATION SHAFT)\s+\d+\s\-\s*(?P<segment>\d+)\s+(?P<title>\S.+)+FORM"
@@ -439,8 +439,11 @@ def parse_file(file_path, gui="", conversion_setting=""):  # Parser
     output_meta_data.update({"ses_version": version})
     ambient_temperature = get_ambient_temperature(lines)
     # Read segment titles from Form 3 and Form 5 and types from form 3
+    INPUT_for_search = INPUT.copy()
     segment_titles, form3_type, form3_pressure = get_titles_and_form3(lines, version)
     output_meta_data['form3_pressure'] = form3_pressure
+    # Determine the Fire Simulation Option
+   
     try:
         form4_df = get_form4(lines)
         if form4_df is not None:
@@ -494,6 +497,7 @@ def parse_file(file_path, gui="", conversion_setting=""):  # Parser
         if m is not None:
              if int(m.group("supplement_option")) in {3, 5}:
                  section_pressure = True
+                 i = len(lines) + 1  # Exit while loop
         i += 1
 
     # Determine if there are abbreviated prints from Form 12.
@@ -773,7 +777,20 @@ def get_form4(lines):
     next_form_rx = INPUT['f5a']  # signals start of form 5 and end of Form 4
     first_line_rx = INPUT["f4_location"]  # Start of Form 4
     key_prefix = 'f4_'
-    form4_data = form_parse(lines, time_rx, next_form_rx, first_line_rx, key_prefix)
+    #Adjust Form 4 input search dictionary based on the Fire Simulation Option
+    rx = INPUT["f1g_fire"]
+    i = 0
+    m = None
+    INPUT_for_search = INPUT.copy()  # Copy the INPUT dictionary to search for Form 4
+    while m is None and i < len(lines):
+        m = rx.search(lines[i])  # Find supplement output option line
+        if m is not None:
+            if int(m.group("fire_simulation_option")) == 0:
+                INPUT_for_search.pop("f4_flame")
+                INPUT_for_search.pop("f4_area")
+            i = len(lines) + 1  # Exit while loop
+        i += 1
+    form4_data = form_parse(lines, time_rx, next_form_rx, first_line_rx, key_prefix, input_search=INPUT_for_search)
     if len(form4_data) > 0:
         form4_df = pd.DataFrame(form4_data)
         form4_df = form4_df.apply(pd.to_numeric, errors="coerce")
@@ -840,7 +857,7 @@ def get_form7c(lines):
     form7c_data = form_parse(lines, time_rx, next_form_rx, first_line_rx, key_prefix)
     return form7c_data
 
-def form_parse(lines, time_rx, next_form_rx, first_line_rx, key_prefix, i=0):
+def form_parse(lines, time_rx, next_form_rx, first_line_rx, key_prefix, i=0,input_search=INPUT):
     end_of_form = False
     form_data = []
     while not end_of_form or i < len(lines):
@@ -852,10 +869,10 @@ def form_parse(lines, time_rx, next_form_rx, first_line_rx, key_prefix, i=0):
         first_line_match = first_line_rx.search(lines[i])
         if first_line_match is not None:
             form_row = {}
-            for key, value in INPUT.items():
+            for key, value in input_search.items():
                 if key_prefix in key:
                     match = value.search(lines[i])
-                    while match is None and i < len(lines):
+                    while match is None and i < len(lines) -1:
                         i += 1
                         match = value.search(lines[i])
                     form_row.update(match.groupdict())
