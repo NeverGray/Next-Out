404915f1b7a306ae787112dbfbe3e93442d27005
Justin M Edenbaum <62964314+NeverGray@users.noreply.github.com>
2025-10-21 18:17:44 -0400

    Latest compile, but still issues with h5 files.

--- compile.ps1
+++ compile.ps1
@@ -15,5 +15,5 @@ Copy-Item "$PSScriptRoot\NO_Icon.ico" "C:\bin\code\"
 # Exclude large unused libraries: matplotlib, scipy, PIL, test frameworks, pandas I/O modules not used
 pyinstaller -F main.py --noconsole --onefile --icon NO_Icon.ico --add-data "NO_Icon.ico;." `
     --exclude matplotlib --exclude scipy --exclude PIL --exclude unittest --exclude test --exclude tests `
-    --hidden-import=h5py
+    --hidden-import=h5py --hidden-import=tables --hidden-import=pandas.io.pytables --collect-all tables --copy-metadata tables
 Rename-Item -Path "C:\Bin\code\dist\main.exe" -NewName "Next-Out.exe"
